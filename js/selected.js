eb12f0bab79a79ff3823e1e63859b5b289c59801
Robin Hossain <robinhossain5261@gmail.com>
2022-08-22 04:49:08 +0600

    One name added list

--- js/selected.js
+++ js/selected.js
@@ -1,1 +1,23 @@
-console.log('selected part connected')
+function addPlayerName(elementId) {
+    const placeUl = document.getElementById(elementId);
+    const li = document.createElement('li');
+    li.innerText = 'Lionel Messi';
+    placeUl.appendChild(li);
+}
+
+
+document.getElementById('added-btn-list').addEventListener('click', function () {
+    addPlayerName('places-list');
+
+
+})
+
+const button = document.querySelector('#added-btn-list');
+
+const disableButton = () => {
+
+    button.disabled = true;
+
+};
+
+button.addEventListener('click', disableButton);
